ea86fdab263d3dfdcdfe77e2c4c7fe78dcbbf412
handaehee93 <hdh8990@naver.com>
2025-09-24 21:12:06 +0900

    activate, deactivate 추가

--- src/main/java/spring/hexa/domain/Member.java
+++ src/main/java/spring/hexa/domain/Member.java
@@ -2,6 +2,8 @@ package spring.hexa.domain;
 
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 public class Member {
 //    회원 Entity의 속성 정의
@@ -11,9 +13,23 @@ public class Member {
     MemberStatus status;
 
     public Member(String email, String nickName, String passwordHash) {
-        this.email = email;
-        this.nickName = nickName;
-        this.passwordHash = passwordHash;
+        this.email = Objects.requireNonNull(email);
+        this.nickName = Objects.requireNonNull(nickName);
+        this.passwordHash = Objects.requireNonNull(passwordHash);
         this.status = MemberStatus.PENDING;
+    }
+
+    public void activate () {
+        if(this.status != MemberStatus.PENDING) {
+            throw new IllegalStateException("PENDING 상태에서만 ACTIVE 상태로 변경할 수 있습니다.");
+        }
+        this.status = MemberStatus.ACTIVE;
+    }
+
+    public void deactivate () {
+        if(this.status != MemberStatus.ACTIVE) {
+            throw new IllegalStateException("ACTIVE 상태에서만 DEACTIVATE 상태로 변경할 수 있습니다.");
+        }
+        this.status = MemberStatus.DEACTIVATE;
     }
 }
--- src/test/java/spring/hexa/domain/MemberTest.java
+++ src/test/java/spring/hexa/domain/MemberTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberTest {
@@ -12,5 +13,37 @@ class MemberTest {
         Member member = new Member("hdh@test.com", "hdh","secret");
 
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+
+    @Test
+    void nullCheck () {
+        assertThatThrownBy(() -> new Member(null,"hdh","secret"))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void activate () {
+        Member member = new Member("hdh@test.com","hdh","secret");
+        member.activate();
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+    }
+
+    @Test
+    void reactivate () {
+        Member member = new Member("hdh@test.com","hdh","secret");
+        member.activate();
+        assertThatThrownBy(() -> {
+            member.activate();
+        }).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void deactivate () {
+        Member member = new Member("hdh@test.com","hdh","secret");
+        member.activate();
+
+        member.deactivate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.DEACTIVATE);
     }
 }
